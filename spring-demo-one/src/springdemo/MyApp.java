6ba7ff0fa4281034ddb34bdd65d1eb558681612c
Jackson <65201077+jacksontaber@users.noreply.github.com>
2023-01-02 22:04:05 -0500

    Rough Prototype

---
+++ spring-demo-one/src/springdemo/MyApp.java
@@ -0,0 +1,17 @@
+package springdemo;
+
+public class MyApp {
+
+	public static void main(String[] args) {
+		
+		Coach theCoach = new BaseballCoach();
+		
+		Coach theTrackCoach = new TrackCoach();
+		
+		System.out.println(theCoach.getDailyWorkout());
+		
+		System.out.println(theTrackCoach.getDailyWorkout());
+
+	}
+
+}
