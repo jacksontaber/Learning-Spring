c29639596e24a7c85f41c21428d8bb9eb5a85edd
Jackson <65201077+jacksontaber@users.noreply.github.com>
2023-01-07 18:08:56 -0500

    Constructor Injection

--- spring-demo-annotations/src/springdemo/AnnotationDemoApp.java
+++ spring-demo-annotations/src/springdemo/AnnotationDemoApp.java
@@ -16,6 +16,9 @@ public class AnnotationDemoApp {
 		// call a method on the bean
 		System.out.println(theCoach.getDailyWorkout());
 		
+		// call method to get fortune
+		System.out.println(theCoach.getDailyFortune());
+		
 		// close the context
 		context.close();
 	}
